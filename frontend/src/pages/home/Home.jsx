309e28fda778bbfefee015dff11dcf0a3f40b959
unknown <sahli.mohamed.contact@gmail.com>
2025-06-02 01:17:42 +0100

    topsellers

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
+import { Topsellers } from './Topsellers'
 
 const Home = () => {
     return (
         <>
             <Banner/>
+            <Topsellers/>
         </>
     )
 }
-
-
 export default Home
