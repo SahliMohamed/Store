7086408d8563257c8681ae89930c8e4f949e6cca
unknown <sahli.mohamed.contact@gmail.com>
2025-06-02 22:49:54 +0100

    loginRegisterUI

--- frontend/src/pages/home/Topsellers.jsx
+++ frontend/src/pages/home/Topsellers.jsx
@@ -30,7 +30,7 @@ export const Topsellers = () => {
     <div className='py-10'>
         <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
         {/* category filtering */}
-        <div className='mb_8 flex items-center'>
+        <div className='mb-8 flex items-center'>
             <select 
             onChange={(e) => setSelectedCategory(e.target.value)}
             name="category" id="category" className='border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none'>
